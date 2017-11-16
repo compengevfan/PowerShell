ad5b4398a9b00a56d87c4c1ece82274509b4ad63
compengevfan <chris.dupree@gmail.com>
2017-11-15 20:45:48 -0500

    Add files via upload

---
+++ DNS-NS-SZ/DNS-NS-Xfer_Check-Fix.ps1
@@ -0,0 +1,168 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter()] [string] $State = "Test",
+    [Parameter()] [string] $DoEmails = "No"
+)
+
+#This script should run on the PDC of each domain.
+#Script assumes that the PDC can properly resolve all Secondary Zones.
+
+#Check to make sure DC is the PDC, if not, send email and halt script.
+
+#1. "DNS-NS_Check-Fix_XferTarget.txt" contains a list of domains that the AD Integrated zones need to be transferred to.
+
+#2. Import this file and use it to connect these domains and get a list of DNS servers.
+
+#3. Add any missing servers to Name Server list and remove any extra.
+
+#4. "DNS-SZ_Check-Fix_ADI.txt" contains a list of AD Integrated zones that should be transferred.
+
+#5. Import this file and ensure all zones are configured to allow transfers to Name Servers.
+    #Script must cycle through all the DNS servers in the domain.
+
+Function SendAnEmail
+{
+    Param ([Parameter(Mandatory=$True)] [string] $emailserver,
+            [Parameter(Mandatory=$True)] [string] $emailTo,
+            [Parameter(Mandatory=$True)] [string] $emailFrom,
+            [Parameter(Mandatory=$True)] [string] $emailSubject,
+            [Parameter(Mandatory=$True)] [string] $emailbody)
+
+    Send-MailMessage -smtpserver $emailServer -to $emailTo -from $emailFrom -subject $emailSubject -body $emailbody
+}
+
+Import-Module ActiveDirectory
+Import-Module DnsServer
+
+cd C:\Jobs
+
+#Global Email Variables
+$SMTPServer = "smtp.ff.p10"
+#$WhoToEmail = "fanatics+IEC@service-now.com"
+$WhoToEmail = "cdupree@fanatics.com"
+$EmaiIsFrom = "DNS@fanatics.com"
+
+#####################################################
+#BEGIN Ensure that the script is running on the PDC. If it's not, exit
+#####################################################
+
+$DomainInfo = Get-ADDomain
+
+$LocalDomain = $DomainInfo.DNSRoot
+
+$myFQDN = (Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain
+
+if ($DomainInfo.PDCEmulator -ne $myFQDN)
+{
+    $Body = "DNS Name Server script is running on a Domain Controller that is not the PDC.`n`nServer is $myFQDN.`nThis script is designed to run on the PDC."
+    if ($DoEmails -eq "Yes") { SendAnEmail -emailserver $SMTPServer -emailTo $WhoToEmail -emailFrom $EmaiIsFrom -emailSubject "Script not executed on PDC!!!" -emailbody $Body }
+    else { $Body }
+
+    exit
+}
+
+#####################################################
+#END Ensure that the script is running on the PDC. If it's not, exit
+#####################################################
+
+#####################################################
+#BEGIN verify the local domain has the proper list of remote name servers for all remote domains.
+#####################################################
+
+#Get a list of the NS records from the current DNS server. Don't include local.
+$LocalNameServerList = Resolve-DnsName -Name $($DomainInfo.DNSRoot) -Type NS -DnsOnly | Where-Object {$_.NameHost -NotLike "*$($DomainInfo.DNSRoot)*" -and $_.Section -eq "Answer"} | Select-Object NameHost
+
+#Write-Host "List of remote name servers on this DNS server"
+#$LocalNameServerList
+
+#Obtain a list of all the domains that will need to get zone transfers from this DNS server
+$TargetDomains = Get-Content .\DNS-NS-Xfer_Check-Fix_XferTarget.txt
+
+#Go through each remote domain so sync name server lists
+foreach ($TargetDomain in $TargetDomains)
+{
+    #Get domain information for the target
+    $TargetDomainInfo = Get-ADDomainController -Discover -DomainName $TargetDomain -Service PrimaryDC -ErrorAction SilentlyContinue
+
+    #Verify that the PDC is able to query the target
+    if ($TargetDomainInfo -eq $null)
+    {
+        $Body = "The PDC, $myFQDN, is unable to query the target domain, $TargetDomain.`nPlease create a method for this PDC to query this domain."
+        if ($DoEmails -eq "Yes" -and $ChangesMade) { SendAnEmail -emailserver $SMTPServer -emailTo $WhoToEmail -emailFrom $EmaiIsFrom -emailSubject "PDC cannot query a target domain!!!" -emailbody $body }
+        if ($DoEmails -ne "Yes" -and $ChangesMade) { $Body }
+    }
+    else
+    {
+        $ChangesMade = $false
+        #Get a list of the DNS servers in the remote domain
+        $RemoteNameServerList = Resolve-DnsName -Name $TargetDomain -Server $($TargetDomainInfo.Name + "." + $TargetDomainInfo.Domain) -Type NS -DnsOnly | Where-Object {$_.NameHost -Like "*$($TargetDomainInfo.Domain)*" -and $_.Section -eq "Answer"} | Select-Object NameHost
+
+        #Check to make sure that all remote DNS servers are in the list of local Name Servers. If missing, add.
+        $Body = "The following NameServers were added to zone $LocalDomain on $myFQDN`n`n"
+        foreach ($RemoteNameServer in $RemoteNameServerList.NameHost)
+        {
+            if ($LocalNameServerList.NameHost -notcontains $RemoteNameServer)
+            {
+                if ($State -eq "Prod") { Add-DnsServerResourceRecord -NameServer $RemoteNameServer -ZoneName $LocalDomain -Name "." -NS }
+                $Body += "$RemoteNameServer`n"
+                $ChangesMade = $true
+            }
+        }
+
+        #Check to make sure that all local Name Servers are in the list of applicable remote Name Servers. If extra, delete.
+        $Body += "`nThe following NameServers were removed from zone $LocalDomain on $myFQDN`n`n"
+        $LocalNameServerListSubset = $LocalNameServerList | Where-Object {$_.NameHost -Like "*$TargetDomain*"}
+        foreach ($LocalNameServer in $LocalNameServerListSubset.NameHost)
+        {
+            if ($RemoteNameServerList.NameHost -notcontains $LocalNameServer)
+            {
+                if ($State -eq "Prod") { Remove-DnsServerResourceRecord -ZoneName $LocalDomain -Name "." -RecordData $LocalNameServer -RRType NS -Force }
+                $Body += "$LocalNameServer`n"
+                $ChangesMade = $true
+            }
+        }
+        if ($DoEmails -eq "Yes" -and $ChangesMade) { SendAnEmail -emailserver $SMTPServer -emailTo $WhoToEmail -emailFrom $EmaiIsFrom -emailSubject "NameServer Updates" -emailbody $Body }
+        if ($DoEmails -ne "Yes" -and $ChangesMade) { $Body }
+    }
+
+    Clear-Variable TargetDomainInfo
+}
+
+#####################################################
+#END verify the local domain has the proper list of remote name servers for all remote domains.
+#####################################################
+
+#####################################################
+#BEGIN making sure ADI zones that need to be transferred are set to allow it.
+#####################################################
+
+#Get a list of all the DNS servers in this domain
+$LocalDNSServers = Resolve-DnsName -Name $($DomainInfo.DNSRoot) -Type NS -DnsOnly | Where-Object {$_.NameHost -Like "*$($DomainInfo.DNSRoot)*" -and $_.Section -eq "Answer"} | Select-Object NameHost
+
+#Import ADI file 
+$ADIDomains = Get-Content .\DNS-NS-Xfer_Check-Fix_ADI.txt
+
+#Cycle through each DNS server and verify that all ADI zones in txt file are set to allow xfer to name servers
+$ChangesMade = $false
+$Body = "The following is a list of changes made to primary zones on DNS servers in $($DomainInfo.DNSRoot)`n`n"
+foreach ($LocalDNSServer in $LocalDNSServers)
+{
+    foreach ($ADIDomain in $ADIDomains)
+    {
+        $PrimaryZone = Get-DnsServerZone -Name $ADIDomain -ComputerName $LocalDNSServer.NameHost
+
+        if ($PrimaryZone.SecureSecondaries -ne "TransferToZoneNameServer")
+        {
+            $ChangesMade = $true
+            if ($State -eq "Prod") { Set-DnsServerPrimaryZone -Name $ADIDomain -ComputerName $LocalDNSServer.NameHost -SecureSecondaries "TransferToZoneNameServer" }
+            $Body += "Zone $ADIDomain on $($LocalDNSServer.NameHost) has been set to allow zone transfer to Name Servers`n"
+        }
+    }
+}
+
+if ($DoEmails -eq "Yes" -and $ChangesMade) { SendAnEmail -emailserver $SMTPServer -emailTo $WhoToEmail -emailFrom $EmaiIsFrom -emailSubject "Zone Transfer Updates" -emailbody $Body }
+if ($DoEmails -ne "Yes" -and $ChangesMade) { $Body }
+
+#####################################################
+#END making sure ADI zones that need to be transferred are set to allow it.
+#####################################################
---
+++ DNS-NS-SZ/DNS-SZ_Check-Fix.ps1
@@ -0,0 +1,154 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter()] [string] $State = "Test",
+    [Parameter()] [string] $DoEmails = "No"
+)
+
+#This script should run on the PDC of each domain.
+#Script assumes that the PDC can properly resolve all Secondary Zones.
+
+#Check to make sure DC is the PDC, if not, send email and halt script.
+
+#1. "DNS-SZ_Check-Fix_SecondaryZones.txt" contains a list of secondary zones that should be on this DNS server. 
+
+#2. Import this file and use it to create the zones if missing. 
+    #Script must cycle through all zones in the list.
+    #Sript must cycle through all the DNS servers in the domain.
+    #If the zone is missing, add it. If the zone exists, ensure the correct masters are in use.
+    #If the zone being created is in ff.wh or fanatics.corp, find the DNS servers local to the site to use as masters.
+    #If the zone being created is in ff.p10, TBD.
+
+Function SendAnEmail
+{
+    Param ([Parameter(Mandatory=$True)] [string] $emailserver,
+            [Parameter(Mandatory=$True)] [string] $emailTo,
+            [Parameter(Mandatory=$True)] [string] $emailFrom,
+            [Parameter(Mandatory=$True)] [string] $emailSubject,
+            [Parameter(Mandatory=$True)] [string] $emailbody)
+
+    Send-MailMessage -smtpserver $emailServer -to $emailTo -from $emailFrom -subject $emailSubject -body $emailbody
+}
+
+Function DetermineSiteCode
+{
+    Param ([Parameter(Mandatory=$True)] [string] $IncomingServerName)
+
+    if ($IncomingServerName -like "???-*") { $SiteCode = $IncomingServerName.Substring(0,3) }
+    else { $SiteCode = $IncomingServerName.Substring(0,4) }
+
+    return $SiteCode
+}
+
+Function DetermineMasterServers
+{
+    Param ([Parameter(Mandatory=$True)] $RemoteDNSServers,
+            [Parameter(Mandatory=$True)] [string] $IncomingSiteCode)
+
+    $MasterServers = @()
+
+    if ($RemoteDNSServers.NameHost -like "*ff.p10") { $MasterServers += "192.168.125.140"; $MasterServers += "192.168.125.141" }
+    else
+    {
+        $FindTheMasters = $RemoteDNSServers | where NameHost -Like "$IncomingSiteCode-*"
+        foreach ($FindTheMaster in $FindTheMasters)
+        {
+            $MasterServers += Resolve-DnsName $FindTheMaster.NameHost | select IPAddress
+        }
+    }
+
+    return $MasterServers
+}
+
+Import-Module ActiveDirectory
+Import-Module DnsServer
+
+cd C:\Jobs
+
+#Global Email Variables
+$SMTPServer = "smtp.ff.p10"
+#$WhoToEmail = "fanatics+IEC@service-now.com"
+$WhoToEmail = "cdupree@fanatics.com"
+$EmaiIsFrom = "DNS@fanatics.com"
+
+$DomainInfo = Get-ADDomain
+
+$myFQDN = (Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain
+
+if ($DomainInfo.PDCEmulator -ne $myFQDN)
+{
+    $Body = "DNS Name Server script is running on a Domain Controller that is not the PDC.`n`nServer is $myFQDN.`nThis script is designed to run on the PDC."
+    if ($DoEmails -eq "Yes") { SendAnEmail -emailserver $SMTPServer -emailTo $WhoToEmail -emailFrom $EmaiIsFrom -emailSubject "Script not executed on PDC!!!" -emailbody $Body }
+    else { $Body }
+
+    exit
+}
+
+#####################################################
+#BEGIN making sure all DNS servers have the correct secondary zones.
+#####################################################
+
+#Get a list of all the DNS servers in this domain
+$LocalDNSServers = Resolve-DnsName -Name $($DomainInfo.DNSRoot) -Type NS -DnsOnly | Where-Object {$_.NameHost -Like "*$($DomainInfo.DNSRoot)*" -and $_.Section -eq "Answer"} | Select-Object NameHost
+
+#Import secondary zones that should be on this DNS server
+$SecondaryZones = Import-Csv .\DNS-SZ_Check-Fix_SecondaryZones.csv
+
+#Cycle through the secondary zones and ensure each DNS server has this zone setup. If zone missing, add. If zone exists, ensure correct masters.
+$ChangesMade = $false
+
+$Body = "The following changes have been made to secondary zones in the $($DomainInfo.DNSRoot) domain.`n`n"
+
+foreach ($SecondaryZone in $SecondaryZones)
+{
+    #Getting a list of DNS servers in the zone to be created. Will be used to find the appropriate master servers.
+    $TargetDomainInfo = Get-ADDomainController -Discover -DomainName $($SecondaryZone.owner) -Service PrimaryDC
+    $RemoteNameServerList = Resolve-DnsName -Name $($SecondaryZone.owner) -Server $($TargetDomainInfo.Name + "." + $TargetDomainInfo.Domain) -Type NS -DnsOnly | Where-Object {$_.NameHost -Like "*$($TargetDomainInfo.Domain)*" -and $_.Section -eq "Answer"} | Select-Object NameHost
+
+    foreach ($LocalDNSServer in $LocalDNSServers)
+    {
+        #Check to see if secondary zone exists, if not, create it with the proper master servers.
+        $CurrentSiteCode = DetermineSiteCode -IncomingServerName $($LocalDNSServer.NameHost)
+        $SecondaryZoneCheck = Get-DnsServerZone -Name $($SecondaryZone.zone) -ComputerName $LocalDNSServer.NameHost -ErrorAction SilentlyContinue
+        $MasterServers = DetermineMasterServers -RemoteDNSServers $RemoteNameServerList -IncomingSiteCode $CurrentSiteCode
+        if ($SecondaryZoneCheck -eq $null)
+        {
+            if ($State -eq "Prod") { Add-DnsServerSecondaryZone -Name $SecondaryZone.zone -ZoneFile $($SecondaryZone.zone + ".dns") -ComputerName $($LocalDNSServer.NameHost) -MasterServers $($MasterServers.IPAddress[0]), $($MasterServers.IPAddress[1]) }
+            $Body += "$($SecondaryZone.zone) has been added as a secondary zone with $($MasterServers.IPAddress) as the master servers on $($LocalDNSServer.NameHost).`n"
+            $ChangesMade = $true
+        }
+        else
+        {
+            #If the secondary zone exists, verify that it has the correct master servers. If not, correct it.
+            $CurrentMasterServers = $SecondaryZoneCheck.MasterServers | select IPAddressToString | sort IPAddressToString
+            $CorrectMasterServers = $MasterServers | sort IPAddress
+
+            if ($($SecondaryZoneCheck.MasterServers.Count) -ne 2 -or $($CurrentMasterServers.IPAddressToString[0]) -ne $($CorrectMasterServers.IPAddress[0]) -or $($CurrentMasterServers.IPAddressToString[1]) -ne $($CorrectMasterServers.IPAddress[1]))
+            {
+                if ($State -eq "Prod") { Set-DnsServerSecondaryZone -Name $SecondaryZone.zone -ComputerName $($LocalDNSServer.NameHost) -MasterServers $($MasterServers.IPAddress[0]), $($MasterServers.IPAddress[1]) }
+                $Body += "$($SecondaryZone.zone) already exists as a secondary zone on $($LocalDNSServer.NameHost). Master servers were incorrect and have been set to $($MasterServers.IPAddress).`n"
+                $ChangesMade = $true
+            }
+            else
+            {
+                #If the secondary zone exists and the master servers are right, verify that the status of the zone is "running".
+                if ($SecondaryZoneCheck.LastZoneTransferResult -ne 0)
+                {
+                    $TempBody = "$($SecondaryZone.zone) on $($LocalDNSServer.NameHost) is not properly updating!!!"
+                    if ($DoEmails -eq "Yes" -and $ChangesMade) { SendAnEmail -emailserver $SMTPServer -emailTo $WhoToEmail -emailFrom $EmaiIsFrom -emailSubject "Secondary Zone Failure!!!" -emailbody $TempBody }
+                    if ($DoEmails -ne "Yes" -and $ChangesMade) { $TempBody }
+                }
+            }
+            #Write-Host "$($SecondaryZone.zone) exists on $($LocalDNSServer.NameHost)"
+            #Write-Host "Site Code is $CurrentSiteCode"
+        }
+
+        Clear-Variable SecondaryZoneCheck
+    }
+}
+
+if ($DoEmails -eq "Yes" -and $ChangesMade) { SendAnEmail -emailserver $SMTPServer -emailTo $WhoToEmail -emailFrom $EmaiIsFrom -emailSubject "Secondary Zone Updates" -emailbody $Body }
+if ($DoEmails -ne "Yes" -and $ChangesMade) { $Body }
+
+#####################################################
+#END making sure all DNS servers have the correct secondary zones.
+#####################################################
