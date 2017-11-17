0cef159b5f53f47ce294819e7502b1eb660a1848
compengevfan <chris.dupree@gmail.com>
2017-11-16 23:54:34 -0500

    Added new function

---
+++ Functions/Function_DoLogging.ps1
@@ -0,0 +1,31 @@
+﻿[CmdletBinding()]
+Param(
+)
+
+Function DoLogging
+{
+    Param(
+        [Parameter(Mandatory=$true)][ValidateSet("Succ","Info","Warn","Err")] [string] $LogType,
+        [Parameter()] [string] $LogString
+    )
+
+    $TimeStamp = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+    "$TimeStamp $LogString" | Out-File .\Logs\$InputFileName + " " + $ScriptStarted + ".txt" -append
+
+    Write-Host -F DarkGray "[" -NoNewLine
+    Write-Host -F Green "*" -NoNewLine
+    Write-Host -F DarkGray "] " -NoNewLine
+    Switch ($LogType)
+    {
+        Succ { Write-Host $LogString -F Green }
+        Info { Write-Host $LogString }
+        Warn { Write-Host -F Yellow $LogString }
+        Err
+        {
+            Write-Host -F Red $LogString
+            Send-MailMessage -smtpserver $emailServer -to $emailTo -from $emailFrom -subject "Cloud-O-Mite Encountered an Error" -body (Get-Content .\Logs\$InputFileName + " " + $ScriptStarted + ".txt")
+        }
+    }
+}
+
+DoLogging 
