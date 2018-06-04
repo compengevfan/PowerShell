bcc4a89a6ef12ac6c43c3d5d6b88fdf4a8ce2dcb
compengevfan <chris.dupree@gmail.com>
2018-06-04 10:59:10 -0400

    update

--- D-Commander/~D-Commander.ps1
+++ D-Commander/~D-Commander.ps1
@@ -1,1 +1,43 @@
-#Coming Soon...
+[CmdletBinding()]
+Param(
+    [Parameter()] [string] $InputFile,
+    [Parameter()] $DomainCredentials = $null
+)
+ 
+$ScriptPath = $PSScriptRoot
+cd $ScriptPath
+ 
+$ScriptStarted = Get-Date -Format MM-dd-yyyy_hh-mm-ss
+$ScriptName = $MyInvocation.MyCommand.Name
+ 
+$ErrorActionPreference = "SilentlyContinue"
+ 
+Function Check-PowerCLI
+{
+    Param(
+    )
+ 
+    if (!(Get-Module -Name VMware.VimAutomation.Core))
+    {
+        write-host ("Adding PowerCLI...")
+        Get-Module -Name VMware* -ListAvailable | Import-Module -Global
+        write-host ("Loaded PowerCLI.")
+    }
+}
+ 
+if (!(Get-Module -ListAvailable -Name DupreeFunctions)) { Write-Host "'DupreeFunctions' module not available!!! Please check with Dupree!!! Script exiting!!!" -ForegroundColor Red; exit }
+if (!(Get-Module -Name DupreeFunctions)) { Import-Module DupreeFunctions }
+if (!(Test-Path .\~Logs)) { New-Item -Name "~Logs" -ItemType Directory | Out-Null }
+ 
+Check-PowerCLI
+Connect-vCenter
+
+cls
+$JSONResponse = Read-Host "Do you already have a completed JSON? (y/n)"
+
+if ($JSONResponse -ne "n" -and $JSONResponse -ne "y") { Write-Host "Invalid response received!!! Script Exiting!!!"; exit }
+
+if ($JSONResponse -eq "n")
+{
+    
+}
