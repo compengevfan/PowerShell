21e35b307e25178463615f994cb258a746fe7218
compengevfan <chris.dupree@gmail.com>
2018-08-03 15:06:54 -0400

    Added Header File

---
+++ ~PowerCLIHeader.ps1
@@ -0,0 +1,40 @@
+[CmdletBinding()]
+Param(
+    [Parameter()] [string] $InputFile,
+    [Parameter()] $CredFile = $null
+)
+ 
+$ScriptPath = $PSScriptRoot
+cd $ScriptPath
+  
+$ScriptStarted = Get-Date -Format MM-dd-yyyy_hh-mm-ss
+$ScriptName = $MyInvocation.MyCommand.Name
+  
+$ErrorActionPreference = "SilentlyContinue"
+  
+Function Check-PowerCLI
+{
+    Param(
+    )
+  
+    if (!(Get-Module -Name VMware.VimAutomation.Core))
+    {
+        write-host ("Adding PowerCLI...")
+        Get-Module -Name VMware* -ListAvailable | Import-Module -Global
+        write-host ("Loaded PowerCLI.")
+    }
+}
+  
+if (!(Get-Module -ListAvailable -Name DupreeFunctions)) { Write-Host "'DupreeFunctions' module not available!!! Please check with Dupree!!! Script exiting!!!" -ForegroundColor Red; exit }
+if (!(Get-Module -Name DupreeFunctions)) { Import-Module DupreeFunctions }
+if (!(Test-Path .\~Logs)) { New-Item -Name "~Logs" -ItemType Directory | Out-Null }
+  
+Check-PowerCLI
+ 
+if ($CredFile -ne $null)
+{
+    Remove-Variable Credential_To_Use -ErrorAction Ignore
+    New-Variable -Name Credential_To_Use -Value $(Import-Clixml $($CredFile))
+}
+ 
+Connect-vCenter -vCenter $vCenter -vCenterCredential $Credential_To_Use
