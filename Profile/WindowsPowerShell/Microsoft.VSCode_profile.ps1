4a482f90074fadb095f9caaa5365e3eac654c5cd
Dupree <Y7537@csx.com>
2020-06-16 12:04:59 -0400

    update

--- Profile/WindowsPowerShell/Microsoft.VSCode_profile.ps1
+++ Profile/WindowsPowerShell/Microsoft.VSCode_profile.ps1
@@ -19,28 +19,26 @@ else { Write-Host "Github path NOT found." -ForegroundColor Yellow}
 if ($env:githome) { $githome = $env:githome; Write-Host "Git path found." -ForegroundColor Green }
 else { Write-Host "Git path NOT found." -ForegroundColor Yellow}
 
-Write-Host "Checking DupreeFunctions module available and latest version..."
-$DupreeFunctionsMinVersion = (Find-Module DupreeFunctions).Version
-if (!(Get-InstalledModule -Name DupreeFunctions -MinimumVersion $DupreeFunctionsMinVersion -ErrorAction SilentlyContinue))
-{
-	try 
-	{
-		if (!(Get-Module -ListAvailable -Name DupreeFunctions)) { Install-Module -Name DupreeFunctions -Scope CurrentUser -Force -ErrorAction Stop }
-		else { Update-Module -Name DupreeFunctions -RequiredVersion $DupreeFunctionsMinVersion -Force -ErrorAction Stop }
-		$DupreeFunctionInstallSuccess = $true
-	}
-	catch { Write-Host "Failed to install 'DupreeFunctions' module from PSGallery!!! Error encountered is:`n`r`t$($Error[0])" -ForegroundColor Red ; $DupreeFunctionInstallSuccess = $false}
-}
-else { $DupreeFunctionInstallSuccess = $true }
+# Write-Host "Checking DupreeFunctions module available and latest version..."
+# $DupreeFunctionsMinVersion = (Find-Module DupreeFunctions).Version
+# if (!(Get-InstalledModule -Name DupreeFunctions -MinimumVersion $DupreeFunctionsMinVersion -ErrorAction SilentlyContinue))
+# {
+# 	try 
+# 	{
+# 		if (!(Get-Module -ListAvailable -Name DupreeFunctions)) { Install-Module -Name DupreeFunctions -Scope CurrentUser -Force -ErrorAction Stop }
+# 		else { Update-Module -Name DupreeFunctions -RequiredVersion $DupreeFunctionsMinVersion -Force -ErrorAction Stop }
+# 		$DupreeFunctionInstallSuccess = $true
+# 	}
+# 	catch { Write-Host "Failed to install 'DupreeFunctions' module from PSGallery!!! Error encountered is:`n`r`t$($Error[0])" -ForegroundColor Red ; $DupreeFunctionInstallSuccess = $false}
+# }
+# else { $DupreeFunctionInstallSuccess = $true }
 
-if (!(Get-Module -Name DupreeFunctions) -and $DupreeFunctionInstallSuccess) { Write-Host "DupreeFunctions Installed and up to date...Importing..." -ForegroundColor Green; Import-Module DupreeFunctions -MinimumVersion $DupreeFunctionsMinVersion }
+# if (!(Get-Module -Name DupreeFunctions) -and $DupreeFunctionInstallSuccess) { Write-Host "DupreeFunctions Installed and up to date...Importing..." -ForegroundColor Green; Import-Module DupreeFunctions -MinimumVersion $DupreeFunctionsMinVersion }
 
-# if (!(Get-Module -ListAvailable -Name DupreeFunctions)) { Write-Host "'DupreeFunctions' module not available!!! Please check with Dupree!!! Script exiting!!!" -ForegroundColor Red; exit }
-# if (!(Get-Module -Name DupreeFunctions)) { Import-Module DupreeFunctions }
-# if (!(Test-Path .\~Logs)) { New-Item -Name "~Logs" -ItemType Directory | Out-Null }
+if (!(Get-Module -ListAvailable -Name DupreeFunctions)) { Write-Host "'DupreeFunctions' module not available." -ForegroundColor Yellow }
+elseif (!(Get-Module -Name DupreeFunctions)) { Import-Module DupreeFunctions }
+if (!(Test-Path .\~Logs)) { New-Item -Name "~Logs" -ItemType Directory | Out-Null }
   
-# Check-PowerCLI
-
 ################
 #Make it pretty#
 ################
