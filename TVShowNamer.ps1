0a54d6daf0c6f41c5d7eff841e7b0a9d3bbd7d09
compengevfan <chris.dupree@gmail.com>
2022-06-05 13:49:50 -0400

    Update TVShowNamer.ps1

--- TVShowNamer.ps1
+++ TVShowNamer.ps1
@@ -4,20 +4,21 @@ param (
     [Parameter()] $ShowName,
     [Parameter()] $Season
 )
+#Requires -Version 7.2
 
-# $Season = "1"
-# $ShowName = "Shadowhunters"
 if ($ShowType -eq "TV1") { $Path = "\\storage1\Media\TV Shows\$ShowName\Season $Season" }
 if ($ShowType -eq "TV2") { $Path = "\\storage2\Media\TV Shows\$ShowName\Season $Season" }
 if ($ShowType -eq "Cartoon") { $Path = "\\storage1\Media\Cartoons\$ShowName\Season $Season" }
 
 $i = 1
 
-$Files = gci $Path | sort Name
+$ToNatural = { [regex]::Replace($_, '\d+', { $args[0].Value.PadLeft(20) }) }
+
+$Files = Get-ChildItem $Path | Sort-Object $ToNatural
 
 foreach ($File in $Files)
 {
-    $Extension = (Get-Item -LiteralPath $Path\$File).Extension
+    $Extension = (Get-Item -LiteralPath $File).Extension
     $NewName = $ShowName + " - " + "s" + $Season + "e" + $i + $Extension
     Rename-Item -LiteralPath $File.FullName -NewName $NewName
     $i += 1
