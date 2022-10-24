4b282a89c009056f6d45c5b31d2041c888bb9b04
compengevfan <chris.dupree@gmail.com>
2022-10-24 19:52:27 -0400

    Netbox functions and DF update

---
+++ DupreeFunctions/Functions/netbox.psm1
@@ -0,0 +1,103 @@
+Function Invoke-NetboxAddVm {
+    Param(
+        [Parameter(Mandatory = $true)] [string] $VMName,
+        [Parameter()] [string] $Status = "active",
+        [Parameter()] [int16] $Site = 1,
+        [Parameter()] [int16] $Cluster = 1,
+        [Parameter(Mandatory = $true)] [int16] $vCPUs,
+        [Parameter(Mandatory = $true)] [int16] $RAM,
+        [Parameter(Mandatory = $true)] [int16] $Disk,
+        [Parameter(Mandatory = $true)] [pscredential] $Credential
+    )
+
+    $NetboxApiToken = $Credential.GetNetworkCredential().Password
+
+    [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Accept", "application/json")
+    $headers.Add("Authorization", "Token $NetboxApiToken")
+    $headers.Add("Content-Type", "application/json")
+
+    $Body = @{"name" = $VMName; "status" = $Status; "site" = $Site; "cluster" = $Cluster; "vcpus" = $vCPUs; "memory" = $($RAM * 1024); "disk" = $Disk }
+    $BodyJSON = $Body | ConvertTo-JSON
+
+    $response = Invoke-RestMethod 'https://jax-nbx001.evorigin.com/api/virtualization/virtual-machines/' -Method 'POST' -Headers $headers -Body $BodyJSON
+    $response | ConvertTo-Json
+}
+
+Function Invoke-NetboxAddVmInterface {
+    Param(
+        [Parameter(Mandatory = $true)] [int16] $VmId,
+        [Parameter(Mandatory = $true)] [string] $Name,
+        [Parameter()] [string] $enabled = "true",
+        [Parameter(Mandatory = $true)] [pscredential] $Credential
+    )
+
+    $NetboxApiToken = $Credential.GetNetworkCredential().Password
+
+    [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Accept", "application/json")
+    $headers.Add("Authorization", "Token $NetboxApiToken")
+    $headers.Add("Content-Type", "application/json")
+
+    $Body = @{ "virtual_machine" = $VmId; "name" = $Name; "enabled" = $enabled }
+    $BodyJSON = $Body | ConvertTo-JSON
+
+    $response = Invoke-RestMethod 'https://jax-nbx001.evorigin.com/api/virtualization/interfaces/' -Method 'POST' -Headers $headers -Body $BodyJSON
+    $response | ConvertTo-Json
+
+}
+
+Function Invoke-NetboxAddIp {
+    Param(
+        [Parameter(Mandatory = $true)] [string] $Address,
+        [Parameter()] [string] $Status = "active",
+        [Parameter()] [string] $AssignedObjectType = "virtualization.vminterface",
+        [Parameter(Mandatory = $true)] [int16] $AssignedObjectId,
+        [Parameter(Mandatory = $true)] [string] $DnsName,
+        [Parameter(Mandatory = $true)] [pscredential] $Credential
+    )
+
+    $NetboxApiToken = $Credential.GetNetworkCredential().Password
+
+    [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Accept", "application/json")
+    $headers.Add("Authorization", "Token $NetboxApiToken")
+    $headers.Add("Content-Type", "application/json")
+
+    $Body = @{ "address" = $Address; "status" = $Status; "assigned_object_type" = $AssignedObjectType; "assigned_object_id" = $AssignedObjectId; "dns_name" = $DnsName }
+    $BodyJSON = $Body | ConvertTo-JSON
+
+    $response = Invoke-RestMethod 'https://jax-nbx001.evorigin.com/api/ipam/ip-addresses/' -Method 'POST' -Headers $headers -Body $BodyJSON
+    $response | ConvertTo-Json
+
+}
+
+Function Invoke-NetboxAddIpToVM {
+    Param(
+        [Parameter(Mandatory = $true)] [string] $VmId,
+        [Parameter(Mandatory = $true)] [int16] $IpId,
+        [Parameter(Mandatory = $true)] [pscredential] $Credential
+    )
+
+    $NetboxApiToken = $Credential.GetNetworkCredential().Password
+
+    [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Accept", "application/json")
+    $headers.Add("Authorization", "Token $NetboxApiToken")
+    $headers.Add("Content-Type", "application/json")
+
+    $Body = @{ "primary_ip4" = $IpId }
+    $BodyJSON = $Body | ConvertTo-JSON
+
+    $response = Invoke-RestMethod "https://jax-nbx001.evorigin.com/api/virtualization/virtual-machines/$VmId/" -Method 'PATCH' -Headers $headers -Body $BodyJSON
+    $response | ConvertTo-Json
+
+}
