0e080300ec9a08f8d261b71c6613eae8ab12d1e8
CompEngLSUFan <chris.dupree@gmail.com>
2022-11-11 13:29:50 -0500

    update

--- Credentials/Import-Credentials.ps1
+++ Credentials/Import-Credentials.ps1
@@ -7,7 +7,7 @@ Remove-Variable Credential-*
 $ComputerName = $env:computername
 #$UserName = $env:USERDOMAIN + "_" + $env:USERNAME
 
-$CredFiles = Get-ChildItem $githubhome\Credentials\Credential-*.xml
+$CredFiles = Get-ChildItem $githome\Credentials\Credential-*.xml
 
 foreach ($CredFile in $CredFiles) {
     New-Variable -Name $CredFile.BaseName -Value $(Import-Clixml $CredFile) -Scope Global
