6df8ab761aecaf8065d9799caf0864934d8be999
CompEngLSUFan <chris.dupree@gmail.com>
2023-01-23 15:46:28 -0500

    changes

--- BalanceDatastoreSpace.ps1
+++ BalanceDatastoreSpace.ps1
@@ -21,14 +21,11 @@ Param(
 )
  
 $ScriptPath = $PSScriptRoot
-cd $ScriptPath
-  
-$ScriptStarted = Get-Date -Format MM-dd-yyyy_HH-mm-ss
-$ScriptName = $MyInvocation.MyCommand.Name
+Set-Location $ScriptPath
   
 $ErrorActionPreference = "SilentlyContinue"
   
-Function Check-PowerCLI
+Function Add-PowerCLI
 {
     Param(
     )
@@ -45,9 +42,9 @@ if (!(Get-Module -ListAvailable -Name DupreeFunctions)) { Write-Host "'DupreeFun
 if (!(Get-Module -Name DupreeFunctions)) { Import-Module DupreeFunctions }
 if (!(Test-Path .\~Logs)) { New-Item -Name "~Logs" -ItemType Directory | Out-Null }
   
-Check-PowerCLI
+Add-PowerCLI
  
-if ($CredFile -ne $null)
+if ($null -ne $CredFile)
 {
     Remove-Variable Credential_To_Use -ErrorAction Ignore
     New-Variable -Name Credential_To_Use -Value $(Import-Clixml $($CredFile))
@@ -68,7 +65,7 @@ $DSmostFree = $DatastoresToBalance | Select-Object -Last 1
 $Space1 = $DSmostFree.FreeSpaceGB
 $Space2 = $DSleastFree.FreeSpaceGB
 $Diff = $Space1 - $Space2
-if ($Diff -gt 30 -and $DatastoresToBalance.Count -gt 1) { $RunAgain = $true }
+if ($Diff -gt 100 -and $DatastoresToBalance.Count -gt 1) { $RunAgain = $true }
 else
 {
     $RunAgain = $false
@@ -84,8 +81,10 @@ while ($RunAgain)
 
     $VMtoMove = $SourceVMs[$RandomNumber]
 
-    $Supress = Move-VM -VM $VMtoMove -Datastore $DSmostFree.Name -Confirm:$false
-    Write-Host ("Migrated $($VMtoMove.Name) from $($DSleastFree.Name) to $($DSmostFree.Name).")
+    Write-Host ("Moving $($VMtoMove.Name) from $($DSleastFree.Name) to $($DSmostFree.Name).")
+    Move-VM -VM $VMtoMove -Datastore $DSmostFree.Name -Confirm:$false | Out-Null
+
+    Start-Sleep 5
 
     #Retrieve datastores from cluster
     $DatastoresToBalance = Get-DatastoreCluster $DatastoreCluster | Get-Datastore | Sort-Object FreeSpaceGB
@@ -98,7 +97,7 @@ while ($RunAgain)
     $Space1 = $DSmostFree.FreeSpaceGB
     $Space2 = $DSleastFree.FreeSpaceGB
     $Diff = $Space1 - $Space2
-    if ($Diff -lt 30)
+    if ($Diff -lt 100)
     {
         $RunAgain = $false
         Write-Host ("Script complete. Cluster is now balanced.")
