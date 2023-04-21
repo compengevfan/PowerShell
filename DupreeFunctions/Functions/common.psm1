5bc2a2949504534f2948df288d08451542375597
CompEngLSUFan <chris.dupree@gmail.com>
2023-04-21 16:58:05 -0400

    saving

--- DupreeFunctions/Functions/common.psm1
+++ DupreeFunctions/Functions/common.psm1
@@ -36,64 +36,6 @@ Function Invoke-Logging {
     }
 }
 
-Function Invoke-SystemSetup {
-    #Check for PowerCLI and version
-    $PowerCLICheck = Get-Module -ListAvailable VMware.Vim
-    if ($null -ne $PowerCLICheck) { Write-Host "`nPowerCLI $($PowerCLICheck.Version.Major).$($PowerCLICheck.Version.Minor) is installed." -ForegroundColor green }
-    else { 
-        Write-Host "`nPowerCLI not found. Installing..." -ForegroundColor red
-        Install-Module VMware.PowerCLI -Scope CurrentUser -Force
-    }
-
-    try {
-        git | Out-Null
-        Write-Host "Git is installed" -ForegroundColor Green
-        if ($env:githome) { 
-            Write-Host "Git environment variable found." -ForegroundColor Green
-            $githome = $env:githome
-            Write-Host "Copying primary profile script using environment variable." -ForegroundColor Green
-            Copy-Item -Path $githome\PowerShell\Profile\Microsoft.PowerShell_profile.ps1 -Destination $PROFILE -Force
-        }
-        else { 
-            Write-Host "Git environment variable NOT found." -ForegroundColor Yellow
-            if (Test-Path C:\git) { $GitPath = "C:\git" } 
-            elseif (Test-Path E:\Dupree\git) { $GitPath = "E:\Dupree\git" }
-            else { $GitPath = Read-Host "Please provide the git path." -ForegroundColor Yellow }
-            Write-Host "Creating Git environment variable." -ForegroundColor Green
-            [System.Environment]::SetEnvironmentVariable('githome', $GitPath, [System.EnvironmentVariableTarget]::User)
-            Write-Host "Copying primary profile script using temporary variable." -ForegroundColor Green
-            Copy-Item -Path $GitPath\PowerShell\Profile\Microsoft.PowerShell_profile.ps1 -Destination $PROFILE -Force
-        }
-        Write-Host "Creating ISE profile script." -ForegroundColor Green
-        Copy-Item -Path $PROFILE -Destination $PROFILE.Replace("Microsoft.PowerShell_profile.ps1", "Microsoft.PowerShellISE_profile.ps1")
-        Write-Host "Copying VS Code profile script." -ForegroundColor Green
-        Copy-Item -Path $PROFILE -Destination $PROFILE.Replace("Microsoft.PowerShell_profile.ps1", "Microsoft.VSCode_profile.ps1")
-    }
-    catch [System.Management.Automation.CommandNotFoundException] {
-        Write-Host "Git install not found" -ForegroundColor red
-    }
-    catch {
-        Write-Host "An error occurred:"
-        Write-Host $_
-    }
-
-    $DropboxProcess = Get-Process -Name Dropbox -ErrorAction SilentlyContinue
-    if ($($DropboxProcess).Count -gt 0) {
-        Write-Host "Dropbox is installed and running." -ForegroundColor Green
-        Write-Host "Checking for Dropbox environment variable..." -ForegroundColor Green
-        if ($env:dropboxhome) { Write-Host "Dropbox environment variable found." -ForegroundColor Green }
-        else {
-            Write-Host "Dropbox environment variable NOT found." -ForegroundColor Yellow
-            if (Test-Path "C:\Cloud\Dropbox") { $GitPath = "C:\Cloud\Dropbox" }
-            else { $GitPath = Read-Host "Please provide the Dropbox path." -ForegroundColor Yellow }
-            Write-Host "Creating Dropbox environment variable." -ForegroundColor Green
-            [System.Environment]::SetEnvironmentVariable('dropboxhome', $GitPath, [System.EnvironmentVariableTarget]::User)
-        }
-    }
-
-    Write-Host "System setup complete. Close this window and open a new one."
-}
-
 Function Save-Credential {
     Param(
         [Parameter()] [string] $Name
