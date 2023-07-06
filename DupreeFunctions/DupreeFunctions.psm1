0a9b321c68bdd8052a00a3ca0ac911b67fd27691
CompEngLSUFan <chris.dupree@gmail.com>
2023-07-05 20:01:47 -0400

    saving

--- DupreeFunctions/DupreeFunctions.psm1
+++ DupreeFunctions/DupreeFunctions.psm1
@@ -2,4 +2,4 @@
 
 New-Alias -Name cvc -Value Connect-vCenter -Scope Global -Force
 New-Alias -Name svc -Value Show-vCenter -Scope Global -Force
-New-Alias -Name redf -Value Connect-vCenter -Scope Global -Force
+New-Alias -Name redf -Value Import-DupreeFunctionsClean -Scope Global -Force
