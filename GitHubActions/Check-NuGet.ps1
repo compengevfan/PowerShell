37e781796722b37aec400d9996c1795b360eb44f
Dupree <chris.dupree@sscinc.com>
2023-07-17 10:22:36 -0400

    [1234567]

--- GitHubActions/Check-NuGet.ps1
+++ GitHubActions/Check-NuGet.ps1
@@ -1,3 +1,5 @@
+#not in use because installing NuGet remotely is a PITA
+
 $destinations = @(
     "jax-pc002.evorigin.com"
 ) | Sort-Object
@@ -5,14 +7,12 @@ $destinations = @(
 foreach ($destination in $destinations) {
     Write-Host "Processing $destination"
     Invoke-Command -ComputerName $destination -ScriptBlock {
-        #Check if NuGet Package Provider is in place
-        $NuGetPPCheck = Get-PackageProvider NuGet
-        if ($null -eq $NuGetPPCheck) {
-            Write-Host "Installing NuGet Package Provider"
-            Install-PackageProvider -Name NuGet -Scope CurrentUser -Force
+        #Check if PowerShell Gallery Repository is set as trusted.
+        $PsgInstallPolicy = Get-PSRepository -Name PSGallery
+        if ($($PsgInstallPolicy.InstallationPolicy) -ne "Trusted") {
+            Write-Host "Setting PSGallery Install Policy to Trusted"
+            Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
         }
-        else {
-            Write-Host "NuGet Package Provider already in place."
-        }
+        else { Write-Host "PSGallery Install Policy already set to Trusted" }
     }
 }
