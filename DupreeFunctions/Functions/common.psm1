866c26b1d5c2809c13983209981520d4ad67ea88
CompEngLSUFan <chris.dupree@gmail.com>
2023-08-23 22:18:58 -0400

    T1234

--- DupreeFunctions/Functions/common.psm1
+++ DupreeFunctions/Functions/common.psm1
@@ -363,5 +363,5 @@ Function Update-DfModuleVersion{
     $PsdContent = Get-Content C:\Git\PowerShell\DupreeFunctions\DupreeFunctions.psd1 -Raw
     $NewPsdContent = $PsdContent.Replace("$OldPsgModuleVersion","$NewPsgModuleVersion")
     $NewPsdContent | Out-File C:\actions-runner\_work\PowerShell\PowerShell\DupreeFunctions\DupreeFunctions.psd1 -Force
-    #T123
+    #T1234
 }
