59b8f1b9209d3397f4de53a3307a2cb353d35ebb
CompEngLSUFan <chris.dupree@gmail.com>
2024-10-12 21:04:00 -0400

    added some code

--- Create-Vm-Json.ps1
+++ Create-Vm-Json.ps1
@@ -22,6 +22,33 @@ Import-DfPowerCLI
 Invoke-DfLogging $LoggingInfoSplat -LogString "Script Started..."
 cvc anthology.evorigin.com
 
+$OperatingSystems = @(
+    "Alma 9"
+    "Windows 10"
+    "Windows 11"
+    "Windows 2016"
+    "Windows 2019"
+    "Windows 2022"
+)
+$OperatingSystemArray = $OperatingSystems | ForEach-Object { [PSCustomObject]@{ OperatingSystem = $_ } }
+$OperatingSystemSelection = Invoke-DfMenu -Objects $OperatingSystemArray -MenuColumn OperatingSystem -SelectionText "Select CPU Count" -ClearScreen:$true
+
+$vCpus = @(2,4,6,8)
+$vCpuArray = $vCpus | ForEach-Object { [PSCustomObject]@{ vCPUs = $_ } }
+$vCpuSelection = Invoke-DfMenu -Objects $vCpuArray -MenuColumn vCPUs -SelectionText "Select CPU Count" -ClearScreen:$true
+
+$Ram = @(2,4,6,8)
+$RamArray = $Ram | ForEach-Object { [PSCustomObject]@{ RAM = $_ } }
+$RamSelection = Invoke-DfMenu -Objects $RamArray -MenuColumn RAM -SelectionText "Select RAM Amount" -ClearScreen:$true
+
+$PortGroupArray = @("JAX-EvOrigin", "Test")
+$PortGroupArray = $Ram | ForEach-Object { [PSCustomObject]@{ PortGroup = $_ } }
+$PortGroupSelection = Invoke-DfMenu -Objects $PortGroupArray -MenuColumn PortGroup -SelectionText "Select a Portgroup" -ClearScreen:$true
+
+$DatastoreArray = @()
+$DatastoreArray = Get-Datastore | Where-Object {$_.Name -notlike "Local-*"} | Sort-Object Name
+$DatastoreSelection = Invoke-DfMenu -Objects $DatastoreArray -MenuColumn Name -SelectionText "Select a Datastore" -ClearScreen:$true
+
 
 
 Invoke-DfLogging $LoggingInfoSplat -LogString "Script Completed Succesfully."
