0fbbea44c2482f2e7c8519d58478f78fa123a4dd
compengevfan <me@you.com>
2024-10-15 21:43:23 -0400

    Creates a new file :)

--- Create-Vm-Json.ps1
+++ Create-Vm-Json.ps1
@@ -9,18 +9,18 @@ Param(
 $ScriptStarted = Get-Date -Format MM-dd-yyyy_HH-mm-ss
 $ScriptName = $MyInvocation.MyCommand.Name
 
-$LoggingSuccSplat = @{ScriptStarted = $ScriptStarted; ScriptName = $ScriptName; LogType = "Succ" }
-$LoggingInfoSplat = @{ScriptStarted = $ScriptStarted; ScriptName = $ScriptName; LogType = "Info" }
-$LoggingWarnSplat = @{ScriptStarted = $ScriptStarted; ScriptName = $ScriptName; LogType = "Warn" }
-$LoggingErrSplat = @{ScriptStarted = $ScriptStarted; ScriptName = $ScriptName; LogType = "Err" }
+# $LoggingSuccSplat = @{ScriptStarted = $ScriptStarted; ScriptName = $ScriptName; LogType = "Succ" }
+# $LoggingInfoSplat = @{ScriptStarted = $ScriptStarted; ScriptName = $ScriptName; LogType = "Info" }
+# $LoggingWarnSplat = @{ScriptStarted = $ScriptStarted; ScriptName = $ScriptName; LogType = "Warn" }
+# $LoggingErrSplat = @{ScriptStarted = $ScriptStarted; ScriptName = $ScriptName; LogType = "Err" }
 
 if (!(Test-Path .\~Logs)) { New-Item -Name "~Logs" -ItemType Directory | Out-Null }
 else { Get-ChildItem .\~Logs | Where-Object CreationTime -LT (Get-Date).AddDays(-30) | Remove-Item }
 
-Import-DfPowerCLI
+# Invoke-DfLogging $LoggingInfoSplat -LogString "Script Started..."
 
-Invoke-DfLogging $LoggingInfoSplat -LogString "Script Started..."
-cvc anthology.evorigin.com
+#Set VM Name to Uppercase
+$VMName = $VMName.ToUpper()
 
 $OperatingSystems = @(
     "Alma 9"
@@ -41,9 +41,9 @@ $Ram = @(2,4,6,8)
 $RamArray = $Ram | ForEach-Object { [PSCustomObject]@{ RAM = $_ } }
 $RamSelection = Invoke-DfMenu -Objects $RamArray -MenuColumn RAM -SelectionText "Select RAM Amount" -ClearScreen:$true
 
-$PortGroupArray = @("JAX-EvOrigin", "Test")
-$PortGroupArray = $Ram | ForEach-Object { [PSCustomObject]@{ PortGroup = $_ } }
-$PortGroupSelection = Invoke-DfMenu -Objects $PortGroupArray -MenuColumn PortGroup -SelectionText "Select a Portgroup" -ClearScreen:$true
+$NetworkArray = @("JAX-EvOrigin", "Test")
+$NetworkArray = $NetworkArray | ForEach-Object { [PSCustomObject]@{ Network = $_ } }
+$NetworkSelection = Invoke-DfMenu -Objects $NetworkArray -MenuColumn Network -SelectionText "Select a Network" -ClearScreen:$true
 
 $Datastores = @(
     "iSCSI-Storage1-NVMe"
@@ -55,5 +55,12 @@ $Datastores = @(
 $DatastoreArray = $Datastores | ForEach-Object { [PSCustomObject]@{ Datastore = $_ } }
 $DatastoreSelection = Invoke-DfMenu -Objects $DatastoreArray -MenuColumn Datastore -SelectionText "Select a Datastore" -ClearScreen:$true
 
+$JsonTemplateContent = Get-Content $githome\vmbuildfiles\V2\~Template.json -Raw
+$NewJsonContent = $JsonTemplateContent.Replace("[OperatingSystem]",$OperatingSystemSelection.OperatingSystem)
+$NewJsonContent = $NewJsonContent.Replace("[vCPUs]",$vCpuSelection.vCPUs)
+$NewJsonContent = $NewJsonContent.Replace("[RAM]",$RamSelection.RAM)
+$NewJsonContent = $NewJsonContent.Replace("[Network]",$NetworkSelection.Network)
+$NewJsonContent = $NewJsonContent.Replace("[Datastore]",$DatastoreSelection.Datastore)
+$NewJsonContent | Out-File "$githome\vmbuildfiles\V2\$VMName.json" -Force
 
-Invoke-DfLogging $LoggingInfoSplat -LogString "Script Completed Succesfully."
+# Invoke-DfLogging $LoggingInfoSplat -LogString "Script Completed Succesfully."
