ac3d39a79c57d3209884570ee919bda3513e0f03
CompEngLSUFan <chris.dupree@gmail.com>
2024-10-28 14:32:14 -0400

    new handbrake batch file. why not?

---
+++ CreateHandbrakeBatchV3.ps1
@@ -0,0 +1,36 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)] [string] $ShowName,
+    [Parameter(Mandatory=$true)] [string] $SourceFolder,
+    [Parameter(Mandatory=$true)] [int] $SubStringCount
+)
+
+#requires -Version 7
+
+$BeginCommand = '"c:\Program Files\Handbrake\HandBrakeCLI" --preset-import-file "C:\Program Files\HandBrake\265-MKV-480.json" -Z "265-MKV-480" --no-dvdnav -i'
+$EndCommand = '.mkv" -m -a "1" -s "scan"'
+
+if (!(Test-Path "C:\Cloud\Dropbox\EpisodeTracker\~V3\$ShowName")) { New-Item "C:\Cloud\Dropbox\EpisodeTracker\~V3\$ShowName" -ItemType Directory } 
+
+$Folders = Get-ChildItem "\\storage1\storage\Downloads\$SourceFolder" | Sort-Object Name
+
+$FolderCounter = 1
+
+foreach ($Folder in $Folders)
+{
+    $BatchFileOutput = ""
+    $files = Get-ChildItem $Folder
+
+    foreach ($file in $files)
+    {
+        $InputLocation = '"' + $file + '"'
+        $OutputLocation = '-o "\\storage3\Media\TV Shows\' + "$ShowName\Season $FolderCounter\" + "$($file.Name.Substring(0,14))"
+        $BatchFileOutput += "$BeginCommand $InputLocation $($Episode.Title) $OutputLocation$EndCommand`r`n"
+    }
+
+    $BatchFileOutput | Out-File $("C:\Cloud\Dropbox\EpisodeTracker\~V3\$ShowName\$ShowName - Season $FolderCounter" + ".bat") -Encoding ascii -Force
+
+    if (!(Test-Path "\\storage3\Media\TV Shows\$ShowName\Season $FolderCounter")) { New-Item "\\storage3\Media\TV Shows\$ShowName\Season $FolderCounter" -ItemType Directory } 
+
+    $FolderCounter++
+}
