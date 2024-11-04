2bf410c12ecea3becea3ec037889c8f8722452c5
CompEngLSUFan <chris.dupree@gmail.com>
2024-11-04 12:47:24 -0500

    Seems good

--- CreateHandbrakeBatchV3.ps1
+++ CreateHandbrakeBatchV3.ps1
@@ -2,12 +2,14 @@
 Param(
     [Parameter(Mandatory=$true)] [string] $ShowName,
     [Parameter(Mandatory=$true)] [string] $SourceFolder,
-    [Parameter(Mandatory=$true)] [int] $SubStringCount
+    [Parameter(Mandatory=$true)] [ValidateSet("480", "720")] [string] $ResolutionTarget,
+    [Parameter()] [int] $SubStringCount,
+    [Parameter()] [switch] $ReuseName
 )
 
 #requires -Version 7
 
-$BeginCommand = '"c:\Program Files\Handbrake\HandBrakeCLI" --preset-import-file "C:\Program Files\HandBrake\265-MKV-480.json" -Z "265-MKV-480" --no-dvdnav -i'
+$BeginCommand = '"c:\Program Files\Handbrake\HandBrakeCLI" --preset-import-file "C:\Program Files\HandBrake\265-MKV-' + $ResolutionTarget + '.json" -Z "265-MKV-' + $ResolutionTarget + '" --no-dvdnav -i'
 $EndCommand = '.mkv" -m -a "1" -s "scan"'
 
 if (!(Test-Path "C:\Cloud\Dropbox\EpisodeTracker\~V3\$ShowName")) { New-Item "C:\Cloud\Dropbox\EpisodeTracker\~V3\$ShowName" -ItemType Directory } 
@@ -18,19 +20,32 @@ $FolderCounter = 1
 
 foreach ($Folder in $Folders)
 {
+    if ($FolderCounter -lt 10){
+        $SeasonNumber = "0" + $FolderCounter
+    }
+    else {
+        $SeasonNumber = $FolderCounter
+    }
+    
     $BatchFileOutput = ""
     $files = Get-ChildItem $Folder
 
     foreach ($file in $files)
     {
+        if ($ReuseName){
+            $OutputFileName = $file.Name.split('.')[0]
+        }
+        else {
+            $OutputFileName = $file.Name.Substring(0,$SubStringCount)
+        }
         $InputLocation = '"' + $file + '"'
-        $OutputLocation = '-o "\\storage3\Media\TV Shows\' + "$ShowName\Season $FolderCounter\" + "$($file.Name.Substring(0,$SubStringCount))"
+        $OutputLocation = '-o "\\storage3\Media\TV Shows\' + "$ShowName\Season $SeasonNumber\" + $OutputFileName
         $BatchFileOutput += "$BeginCommand $InputLocation $($Episode.Title) $OutputLocation$EndCommand`r`n"
     }
 
-    $BatchFileOutput | Out-File $("C:\Cloud\Dropbox\EpisodeTracker\~V3\$ShowName\$ShowName - Season $FolderCounter" + ".bat") -Encoding ascii -Force
+    $BatchFileOutput | Out-File $("C:\Cloud\Dropbox\EpisodeTracker\~V3\$ShowName\$ShowName - Season $SeasonNumber" + ".bat") -Encoding ascii -Force
 
-    if (!(Test-Path "\\storage3\Media\TV Shows\$ShowName\Season $FolderCounter")) { New-Item "\\storage3\Media\TV Shows\$ShowName\Season $FolderCounter" -ItemType Directory } 
+    if (!(Test-Path "\\storage3\Media\TV Shows\$ShowName\Season $SeasonNumber")) { New-Item "\\storage3\Media\TV Shows\$ShowName\Season $SeasonNumber" -ItemType Directory } 
 
     $FolderCounter++
 }
