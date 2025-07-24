93271a33faf4acfcaad19153d0fa9a1b2a00ae31
CompEngLSUFan <chris.dupree@gmail.com>
2025-07-23 22:37:38 -0400

    Enhance playlist import script with improved logging for matching tracks and responses

--- Import-PlexPlaylistV2.ps1
+++ Import-PlexPlaylistV2.ps1
@@ -36,10 +36,10 @@ foreach ($Entry in $Playlist) {
     # Filter and extract ratingKeys
     $matchingKeys = $MusicLibrary.MediaContainer.Metadata | Where-Object { $_.grandparentTitle -eq $Entry.artist -and $_.title -eq $Entry.song -and $_.parentTitle -eq $Entry.album } | Select-Object -ExpandProperty ratingKey
 
-    if ($matchingKeys.count -eq 0) { Write-Host "No match found for artist '$($Entry.artist)' and song '$($Entry.song)' on '$($Entry.album)'"}
-    if ($matchingKeys.count -gt 1) { Write-Host "Found $($matchingKeys.count) matches for artist '$($Entry.artist)' and song '$($Entry.song)'" }
+    if ($matchingKeys.count -eq 0) { Write-Host "No match found for artist '$($Entry.artist)' and song '$($Entry.song)' on '$($Entry.album)'" -ForegroundColor Red }
+    if ($matchingKeys.count -gt 1) { Write-Host "Found $($matchingKeys.count) matches for artist '$($Entry.artist)' and song '$($Entry.song)'" -ForegroundColor Yellow }
     if ($matchingKeys.count -eq 1) { 
-        Write-Debug "ratingKey found for artist '$($Entry.artist)' and song '$($Entry.song)': $($matchingKeys[0])"
+        Write-Verbose "ratingKey found for artist '$($Entry.artist)' and song '$($Entry.song)': $matchingKeys"
         $Tracks += $matchingKeys
     }
 }
@@ -68,4 +68,5 @@ $PlaylistKey = $Response.MediaContainer.Playlist.ratingKey
 Write-Host "Adding tracks to playlist '$PlaylistName'"
 foreach ($Track in $Tracks) {
     $Response = Invoke-PlexRequest -Method "Put" -Endpoint "/playlists/$PlaylistKey/items?uri=server://$MachineID/com.plexapp.plugins.library/library/metadata/$Track"
+    Write-Verbose "Response for adding ratingKey $Track is: $($Response | ConvertTo-Json -Depth 3)"
 }    
