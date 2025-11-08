fa541179af72f39b9b18c6cf0abd9854a14e4a77
compengevfan <chris.dupree@gmail.com>
2025-11-08 16:33:03 -0500

    Enhance Get-SnmpData function to include detailed SNMP metrics for various storage types

--- DupreeFunctions/Functions/common.psm1
+++ DupreeFunctions/Functions/common.psm1
@@ -459,11 +459,49 @@ Function Invoke-UserSetup {
 Function Get-SnmpData {
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory = $true)] [string] $ServerName,
-        [Parameter(Mandatory = $true)] [string] $OIDFileName
     )
 
-    $OIDs = Import-Csv ~./snmp_files/$OIDFileName
+    #Storage1
+    #NVMe - NFS
+    $Storage1_NVMe_Health = snmpget -v 2c -c public -Oqv storage1.evorigin.com .1.3.6.1.4.1.50536.1.1.1.1.3.4
+    $Storage1_NVMe_AvailableSpace = [int64](snmpget -v 2c -c public -Oqv storage1.evorigin.com .1.3.6.1.4.1.50536.1.2.1.1.4.4)
+    $Storage1_NVMe_UsedSpace = [int64](snmpget -v 2c -c public -Oqv storage1.evorigin.com .1.3.6.1.4.1.50536.1.2.1.1.3.4)
+    $Storage1_NVMe_UsedPercent = (($Storage1_NVMe_UsedSpace / ($Storage1_NVMe_AvailableSpace + $Storage1_NVMe_UsedSpace)) * 100)
 
-    
+    #SSD - NFS
+    $Storage1_SSD_Health = snmpget -v 2c -c public -Oqv storage1.evorigin.com .1.3.6.1.4.1.50536.1.1.1.1.3.5
+    $Storage1_SSD_AvailableSpace = [int64](snmpget -v 2c -c public -Oqv storage1.evorigin.com .1.3.6.1.4.1.50536.1.2.1.1.4.5)
+    $Storage1_SSD_UsedSpace = [int64](snmpget -v 2c -c public -Oqv storage1.evorigin.com .1.3.6.1.4.1.50536.1.2.1.1.3.5)
+    $Storage1_SSD_UsedPercent = (($Storage1_SSD_UsedSpace / ($Storage1_SSD_AvailableSpace + $Storage1_SSD_UsedSpace)) * 100)
+
+    #HDD - Storage
+    $Storage1_HDD_Health = snmpget -v 2c -c public -Oqv storage1.evorigin.com .1.3.6.1.4.1.50536.1.1.1.1.3.2
+    $Storage1_HDD_AvailableSpace = [int64](snmpget -v 2c -c public -Oqv storage1.evorigin.com .1.3.6.1.4.1.50536.1.2.1.1.4.2)
+    $Storage1_HDD_UsedSpace = [int64](snmpget -v 2c -c public -Oqv storage1.evorigin.com .1.3.6.1.4.1.50536.1.2.1.1.3.2)
+    $Storage1_HDD_UsedPercent = (($Storage1_HDD_UsedSpace / ($Storage1_HDD_AvailableSpace + $Storage1_HDD_UsedSpace)) * 100)
+
+    #HDD2 - Media Backup
+    $Storage1_HDD2_Health = snmpget -v 2c -c public -Oqv storage1.evorigin.com .1.3.6.1.4.1.50536.1.1.1.1.3.3
+    $Storage1_HDD2_AvailableSpace = [int64](snmpget -v 2c -c public -Oqv storage1.evorigin.com .1.3.6.1.4.1.50536.1.2.1.1.4.3)
+    $Storage1_HDD2_UsedSpace = [int64](snmpget -v 2c -c public -Oqv storage1.evorigin.com .1.3.6.1.4.1.50536.1.2.1.1.3.3)
+    $Storage1_HDD2_UsedPercent = (($Storage1_HDD2_UsedSpace / ($Storage1_HDD2_AvailableSpace + $Storage1_HDD2_UsedSpace)) * 100)
+
+    #Storage3
+    #Volume1 - NFS
+    $Storage3_Vol1_Health = snmpget -v 2c -c public -Oqv storage3.evorigin.com .1.3.6.1.4.1.44738.5.1.1.4.1
+    $Storage3_Vol1_TotalSize = [int64](snmpget -v 2c -c public -Oqv storage3.evorigin.com .1.3.6.1.4.1.44738.5.1.1.6.1)
+    $Storage3_Vol1_FreeSize = [int64](snmpget -v 2c -c public -Oqv storage3.evorigin.com .1.3.6.1.4.1.44738.5.1.1.7.1)
+    $Storage3_Vol1_UsedPercent = ((($Storage3_Vol1_TotalSize - $Storage3_Vol1_FreeSize) / $Storage3_Vol1_TotalSize) * 100)
+
+    #Volume2 - Storage
+    $Storage3_Vol2_Health = snmpget -v 2c -c public -Oqv storage3.evorigin.com .1.3.6.1.4.1.44738.5.1.1.4.2
+    $Storage3_Vol2_TotalSize = [int64](snmpget -v 2c -c public -Oqv storage3.evorigin.com .1.3.6.1.4.1.44738.5.1.1.6.2)
+    $Storage3_Vol2_FreeSize = [int64](snmpget -v 2c -c public -Oqv storage3.evorigin.com .1.3.6.1.4.1.44738.5.1.1.7.2)
+    $Storage3_Vol2_UsedPercent = ((($Storage3_Vol2_TotalSize - $Storage3_Vol2_FreeSize) / $Storage3_Vol2_TotalSize) * 100)
+
+    #Volume3 - Media
+    $Storage3_Vol3_Health = snmpget -v 2c -c public -Oqv storage3.evorigin.com .1.3.6.1.4.1.44738.5.1.1.4.3
+    $Storage3_Vol3_TotalSize = [int64](snmpget -v 2c -c public -Oqv storage3.evorigin.com .1.3.6.1.4.1.44738.5.1.1.6.3)
+    $Storage3_Vol3_FreeSize = [int64](snmpget -v 2c -c public -Oqv storage3.evorigin.com .1.3.6.1.4.1.44738.5.1.1.7.3)
+    $Storage3_Vol3_UsedPercent = ((($Storage3_Vol3_TotalSize - $Storage3_Vol3_FreeSize) / $Storage3_Vol3_TotalSize) * 100)
 }
