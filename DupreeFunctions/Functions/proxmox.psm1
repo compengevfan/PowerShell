4ad752480048a9af0457aefee0ee3bde9ee19ede
CompEngLSUFan <chris.dupree@gmail.com>
2026-02-04 20:29:38 -0500

    Refactor Invoke-DfProxmoxBalanceHosts function to streamline VM migration logic and improve cluster balancing checks

--- DupreeFunctions/Functions/proxmox.psm1
+++ DupreeFunctions/Functions/proxmox.psm1
@@ -32,50 +32,9 @@ function Invoke-DfProxmoxBalanceHosts {
     # if (!(Get-Module -ListAvailable -Name DupreeFunctions)) { Write-Host "'DupreeFunctions' module not available!!! Please check with Dupree!!! Script exiting!!!" -ForegroundColor Red; exit }
     # if (!(Get-Module -Name DupreeFunctions)) { Import-Module DupreeFunctions }
     # if (!(Test-Path .\~Logs)) { New-Item -Name "~Logs" -ItemType Directory | Out-Null }
+    $balanced = $false
 
-    #Determine if cluster needs balancing
-    $clusterResponse = Invoke-DfProxmoxRequest -ProxmoxServer "pmx1.evorigin.com" -ProxmoxToken $ProxmoxToken -Method "GET" -Endpoint "/api2/json/nodes"
-    $ProxmoxNodes = $clusterResponse.data
-    $ProxmoxNodesSorted = $ProxmoxNodes | Sort-Object -Property mem
-
-    $leastMemNode = $ProxmoxNodesSorted[0]
-    $mostMemNode = $ProxmoxNodesSorted[-1]
-
-    Write-Host "Least Memory Node: $($leastMemNode.node) with $([math]::Round($leastMemNode.mem / 1GB, 2)) GB used"
-    Write-Host "Most Memory Node: $($mostMemNode.node) with $([math]::Round($mostMemNode.mem / 1GB, 2)) GB used"
-
-    $Space1 = $mostMemNode.mem
-    $Space2 = $leastMemNode.mem
-    $Diff = $Space1 - $Space2
-    if ($Diff -gt 4294967296) { $RunAgain = $true }
-    else {
-        $RunAgain = $false
-        Write-Host ("Exiting script. Cluster is balanced.")
-    }
-
-    while ($RunAgain) {
-        #Get list of VMs on host with most memory used and pick a random VM
-        $SourceVMs = (Invoke-DfProxmoxRequest -ProxmoxServer "pmx1.evorigin.com" -ProxmoxToken $ProxmoxToken -Method "GET" -Endpoint "/api2/json/nodes/$($mostMemNode.node)/qemu").data | Where-Object { $_.status -eq "running" }
-        $RandomNumber = Get-Random -Maximum $($SourceVMs.Count)
-
-        $VMtoMove = $SourceVMs[$RandomNumber]
-        
-        Write-Host "Migrating VM ID $($VMtoMove.vmid) from $($mostMemNode.node) to $($leastMemNode.node)."
-        $migrateResponse = Invoke-DfProxmoxRequest -ProxmoxServer "pmx1.evorigin.com" -ProxmoxToken $ProxmoxToken -Method "Post" -Endpoint "/api2/json/nodes/$($mostMemNode.node)/qemu/$($VMtoMove.vmid)/migrate?target=$($leastMemNode.node)&online=1"
-
-        Write-Host "Waiting for migration task to complete."
-        $migrationStatus = "notDone"
-        while ($migrationStatus -eq "notDone") {
-            Start-Sleep 5
-            $taskResponse = Invoke-DfProxmoxRequest -ProxmoxServer "pmx1.evorigin.com" -ProxmoxToken $ProxmoxToken -Method "Get" -Endpoint "/api2/json/nodes/$($mostMemNode.node)/tasks/$($migrateResponse.data)/status"
-            if ( $taskResponse.data.status -eq "stopped" ) { $migrationStatus = "Done" }
-        }
-
-        if ($taskResponse.data.exitstatus -eq "OK") { Write-Host "VM migration completed successfully." }
-        else { Write-Host "VM migration encountered a problem. Exit status: $($taskResponse.data.exitstatus)" -ForegroundColor Red; throw }
-
-        Start-Sleep 5
-
+    while (!$balanced) {
         #Determine if cluster still needs balancing
         $response = Invoke-DfProxmoxRequest -ProxmoxServer "pmx1.evorigin.com" -ProxmoxToken $ProxmoxToken -Method "GET" -Endpoint "/api2/json/nodes"
         $ProxmoxNodes = $response.data
@@ -90,20 +49,65 @@ function Invoke-DfProxmoxBalanceHosts {
         $Space1 = $mostMemNode.mem
         $Space2 = $leastMemNode.mem
         $Diff = $Space1 - $Space2
-        if ($Diff -gt 4294967296) { $RunAgain = $true }
+        if ($Diff -gt 4294967296) {
+            #Get list of VMs on host with most memory used and pick a random VM
+            $SourceVMs = (Invoke-DfProxmoxRequest -ProxmoxServer "pmx1.evorigin.com" -ProxmoxToken $ProxmoxToken -Method "GET" -Endpoint "/api2/json/nodes/$($mostMemNode.node)/qemu").data | Where-Object { $_.status -eq "running" }
+            $RandomNumber = Get-Random -Maximum $($SourceVMs.Count)
+
+            $VMtoMove = $SourceVMs[$RandomNumber]
+        
+            Write-Host "Migrating VM ID $($VMtoMove.vmid) from $($mostMemNode.node) to $($leastMemNode.node)."
+            $migrateResponse = Invoke-DfProxmoxRequest -ProxmoxServer "pmx1.evorigin.com" -ProxmoxToken $ProxmoxToken -Method "Post" -Endpoint "/api2/json/nodes/$($mostMemNode.node)/qemu/$($VMtoMove.vmid)/migrate?target=$($leastMemNode.node)&online=1"
+
+            Write-Host "Waiting for migration task to complete."
+            $migrationStatus = "notDone"
+            while ($migrationStatus -eq "notDone") {
+                Start-Sleep 5
+                $taskResponse = Invoke-DfProxmoxRequest -ProxmoxServer "pmx1.evorigin.com" -ProxmoxToken $ProxmoxToken -Method "Get" -Endpoint "/api2/json/nodes/$($mostMemNode.node)/tasks/$($migrateResponse.data)/status"
+                if ( $taskResponse.data.status -eq "stopped" ) { $migrationStatus = "Done" }
+            }
+
+            if ($taskResponse.data.exitstatus -eq "OK") { Write-Host "VM migration completed successfully." }
+            else { Write-Host "VM migration encountered a problem. Exit status: $($taskResponse.data.exitstatus)" -ForegroundColor Red; throw }
+
+            Start-Sleep 5 
+        }
         else {
-            $RunAgain = $false
+            $balanced = $true
             Write-Host ("Exiting script. Cluster is balanced.")
         }
-        # $RunAgain = $false
     }
 }
 
 function Invoke-DfProxmoxEvacuateHost {
     [CmdletBinding()]
     Param(
+        [Parameter(Mandatory = $true)][ValidateSet("pmx1", "pmx2", "pmx3")] [string] $HostToDrain,
         [Parameter(Mandatory = $true)] [string] $ProxmoxToken
     )
 
-    
+    $VMsToMigrate = (Invoke-DfProxmoxRequest -ProxmoxServer "pmx1.evorigin.com" -ProxmoxToken $ProxmoxToken -Method "GET" -Endpoint "/api2/json/nodes/$HostToDrain/qemu").data | Where-Object { $_.status -eq "running" }
+    $MigrationTargets = (Invoke-DfProxmoxRequest -ProxmoxServer "pmx1.evorigin.com" -ProxmoxToken $ProxmoxToken -Method "GET" -Endpoint "/api2/json/nodes").data | Where-Object { $_.node -ne $HostToDrain }
+
+    foreach ($VM in $VMsToMigrate) {
+        #Select target with most free memory
+        $MigrationTargetsSorted = $MigrationTargets | Sort-Object -Property mem
+        $TargetNode = $MigrationTargetsSorted[0]
+
+        Write-Host "Migrating VM ID $($VM.vmid) from $HostToDrain to $($TargetNode.node)."
+        $migrateResponse = Invoke-DfProxmoxRequest -ProxmoxServer "pmx1.evorigin.com" -ProxmoxToken $ProxmoxToken -Method "Post" -Endpoint "/api2/json/nodes/$HostToDrain/qemu/$($VM.vmid)/migrate?target=$($TargetNode.node)&online=1"
+
+        Write-Host "Waiting for migration task to complete."
+        $migrationStatus = "notDone"
+        while ($migrationStatus -eq "notDone") {
+            Start-Sleep 5
+            $taskResponse = Invoke-DfProxmoxRequest -ProxmoxServer "pmx1.evorigin.com" -ProxmoxToken $ProxmoxToken -Method "Get" -Endpoint "/api2/json/nodes/$HostToDrain/tasks/$($migrateResponse.data)/status"
+            if ( $taskResponse.data.status -eq "stopped" ) { $migrationStatus = "Done" }
+        }
+
+        if ($taskResponse.data.exitstatus -eq "OK") { Write-Host "VM migration completed successfully." }
+        else { Write-Host "VM migration encountered a problem. Exit status: $($taskResponse.data.exitstatus)" -ForegroundColor Red; throw }
+
+        Start-Sleep 5 
+    }
 }
